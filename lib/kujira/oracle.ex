5d85530be914eb3e98b9d6bfbec119c58c303d00
codehans <94654388+codehans@users.noreply.github.com>
2024-02-16 10:08:17 +0000

    memoize oracle

--- lib/kujira/oracle.ex
+++ lib/kujira/oracle.ex
@@ -10,42 +10,67 @@ defmodule Kujira.Oracle do
   alias Kujira.Oracle.QueryExchangeRatesResponse
 
   @doc """
-  Loads the price for a specfic price from the on-chain oracle
+  Loads the price for a specfic price from the on-chain oracle. Default cache 200ms
+
+  Clear wth Kujira.Oracle.invalidate(:load_price, denom)
   """
   @spec load_price(GRPC.Channel.t(), any()) :: {:ok, Decimal.t()} | :error
   def load_price(channel, denom) do
-    with {:ok, %QueryExchangeRateResponse{exchange_rate: exchange_rate}} <-
-           Stub.exchange_rate(channel, QueryExchangeRateRequest.new(denom: denom)),
-         {rate, ""} <- Decimal.parse(exchange_rate) do
-      {:ok, Decimal.div(rate, Decimal.new(10 ** 18))}
-    else
-      _ ->
-        :error
-    end
+    Memoize.Cache.get_or_run(
+      {__MODULE__, :load_prices, []},
+      fn ->
+        with {:ok, %QueryExchangeRateResponse{exchange_rate: exchange_rate}} <-
+               Stub.exchange_rate(channel, QueryExchangeRateRequest.new(denom: denom)),
+             {rate, ""} <- Decimal.parse(exchange_rate) do
+          {:ok, Decimal.div(rate, Decimal.new(10 ** 18))}
+        else
+          _ ->
+            :error
+        end
+      end,
+      expires_in: 200
+    )
   end
 
+  @doc """
+  Loads all prices from the on-chain oracle. Default cache 200ms
+
+  Clear wth Kujira.Oracle.invalidate(:load_prices)
+  """
   @spec load_prices(GRPC.Channel.t()) :: {:ok, map()} | :error
   def load_prices(channel) do
-    with {:ok, %QueryExchangeRatesResponse{exchange_rates: rates}} <-
-           Stub.exchange_rates(channel, QueryExchangeRatesRequest.new()),
-         {:ok, rates} <-
-           Enum.reduce(rates, {:ok, %{}}, fn
-             el, {:ok, acc} ->
-               case Decimal.parse(el.amount) do
-                 {dec, ""} ->
-                   {:ok, Map.put(acc, el.denom, Decimal.div(dec, Decimal.new(10 ** 18)))}
+    Memoize.Cache.get_or_run(
+      {__MODULE__, :load_prices, []},
+      fn ->
+        with {:ok, %QueryExchangeRatesResponse{exchange_rates: rates}} <-
+               Stub.exchange_rates(channel, QueryExchangeRatesRequest.new()),
+             {:ok, rates} <-
+               Enum.reduce(rates, {:ok, %{}}, fn
+                 el, {:ok, acc} ->
+                   case Decimal.parse(el.amount) do
+                     {dec, ""} ->
+                       {:ok, Map.put(acc, el.denom, Decimal.div(dec, Decimal.new(10 ** 18)))}
 
-                 _ ->
-                   :error
-               end
+                     _ ->
+                       :error
+                   end
 
-             _, err ->
-               err
-           end) do
-      {:ok, rates}
-    else
-      _ ->
-        :error
-    end
+                 _, err ->
+                   err
+               end) do
+          {:ok, rates}
+        else
+          _ ->
+            :error
+        end
+      end,
+      expires_in: 200
+    )
   end
+
+  def invalidate(:load_price),
+    do: Memoize.invalidate(__MODULE__, :load_price, [])
+
+  def invalidate(:load_prices, denom),
+    do: Memoize.invalidate(__MODULE__, :load_prices, [denom])
 end
