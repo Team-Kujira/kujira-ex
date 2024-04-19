831a6ba973e7531b56b6a86833466e9a7ed89e44
codehans <94654388+codehans@users.noreply.github.com>
2024-04-19 16:15:47 +0100

    ...

--- lib/kujira/fin/book.ex
+++ lib/kujira/fin/book.ex
@@ -112,7 +112,7 @@ defmodule Kujira.Fin.Book do
       consumed = Decimal.div(amount, price)
       remaining = 0
       # ask = %{ask | total: total - consumed}
-      simulate_market_order(%{book | asks: [ask | asks]}, 0, :buy)
+      # simulate_market_order(%{book | asks: [ask | asks]}, 0, :buy)
     end
   end
 
